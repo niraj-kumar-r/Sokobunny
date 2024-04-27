6830e95a389b0ca85be702ba54838edfb2636c3b
artemisia <arvind.srinivasn@gmail.com>
2024-04-27 17:21:54 +0530

    Changes to the representation

--- sokoban.py
+++ sokoban.py
@@ -81,7 +81,7 @@ class Board:
         return set(boxes) == set(trgts)
 
     def __repr__(self):
-        return '\n'.join(''.join(row) for row in self.brd)
+        return '\n'.join(''.join(row) for row in self.brd)+'\n'
 
 
 def soko_solver(board: list[str]):
